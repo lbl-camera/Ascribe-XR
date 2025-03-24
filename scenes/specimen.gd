d636630d962f756e63f38b0088862232846b27d1
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-03-23 20:56:22 -0700

    add ui property to specimen

--- scenes/specimen.gd
+++ scenes/specimen.gd
@@ -7,3 +7,8 @@ enum ScaleMode {TABLE, WORLD}
 
 @export var thumbnail: Texture2D
 @export var scale_mode: ScaleMode = ScaleMode.TABLE
+@export var ui: PackedScene 
+var ui_instance: Control
+
+func _enter_tree() -> void:
+    ui_instance = $/root/Main/SpecimenUIViewport.get_scene_instance()
