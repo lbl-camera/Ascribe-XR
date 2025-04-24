cda22d8d065504bde38fee729ebd35acac078f2c
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-04-24 14:37:07 -0700

    add plug config

---
+++ plug.gd
@@ -0,0 +1,13 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+    # plug("imjp94/gd-YAFSM") # By default, gd-plug will only install anything from "addons/" directory
+    # plug("imjp94/gd-YAFSM", {"include": ["addons/"]})
+    plug("GodotVR/godot-xr-tools", {"commit": "4d53140015ba5feb6f6ec07873f050311ffe1fdc"})
+    plug("Cafezinhu/godot-vr-simulator", {"commit": "5bedbbacf6fe40af10e0bfea99487c84387b19f3"})
+    plug("goatchurchprime/godot-mqtt")
+    plug("goatchurchprime/godot_multiplayer_networking_workbench", {"include":["addons/player-networking"]})
+    plug("Godot-Dojo/Godot-XR-AH", {"include":["addons/xr-autohandtracker"]})
+
+    # large binary addons unpacked and put into a spare repo for the moment
+    var stashedaddons = ["addons/twovoip", "addons/webrtc"]
