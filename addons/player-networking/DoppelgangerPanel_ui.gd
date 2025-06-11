b79bb631335673acfc6a275be3c9e79191ee7744
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-06-11 13:10:42 -0700

    remove plug-sourced addons from git; update embedded addons

--- addons/player-networking/DoppelgangerPanel_ui.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func getnetoffset():
-	return float($hbox/VBox_offset/netoffset.text)*0.001
-
-func seteditable(b):
-	$hbox/VBox_offset/netoffset.editable = b
-	$hbox/VBox_delaymin/netdelaymin.editable = b
--- addons/terrain_3d/tools/importer.gd
+++ addons/terrain_3d/tools/importer.gd
@@ -1,3 +1,5 @@
+# Copyright © 2025 Cory Petkovsek, Roope Palmroos, and Contributors.
+# Importer for Terrain3D
 @tool
 extends Terrain3D
 
@@ -8,27 +10,31 @@ extends Terrain3D
 
 
 func reset_settings(p_value) -> void:
-    if p_value:
-        height_file_name = ""
-        control_file_name = ""
-        color_file_name = ""
-        destination_directory = ""
-        import_position = Vector2i.ZERO
-        height_offset = 0.0
-        import_scale = 1.0
-        r16_range = Vector2(0, 1)
-        r16_size = Vector2i(1024, 1024)
-        material = null
-        assets = null
+	if p_value:
+		height_file_name = ""
+		control_file_name = ""
+		color_file_name = ""
+		destination_directory = ""
+		import_position = Vector2i.ZERO
+		height_offset = 0.0
+		import_scale = 1.0
+		r16_range = Vector2(0, 1)
+		r16_size = Vector2i(1024, 1024)
+		material = null
+		assets = null
+		reset_terrain(true)
 
 
 func reset_terrain(p_value) -> void:
-    data_directory = ""
+	data_directory = ""
+	for region:Terrain3DRegion in data.get_regions_active():
+		data.remove_region(region, false)
+	data.update_maps(Terrain3DRegion.TYPE_MAX, true, false)
 
 
 func update_heights(p_value) -> void:
-    if p_value and data:
-        data.update_height_range()
+	if p_value and data:
+		data.update_height_range()
 
 
 @export_group("Import File")
@@ -47,46 +53,46 @@ func update_heights(p_value) -> void:
 
 
 func set_import_position(p_value: Vector2i) -> void:
-    import_position.x = clamp(p_value.x, -8192, 8192)
-    import_position.y = clamp(p_value.y, -8192, 8192)
+	import_position.x = clamp(p_value.x, -8192, 8192)
+	import_position.y = clamp(p_value.y, -8192, 8192)
 
 
 func set_r16_size(p_value: Vector2i) -> void:
-    r16_size.x = clamp(p_value.x, 0, 16384)
-    r16_size.y = clamp(p_value.y, 0, 16384)
+	r16_size.x = clamp(p_value.x, 0, 16384)
+	r16_size.y = clamp(p_value.y, 0, 16384)
 
 
 func start_import(p_value: bool) -> void:
-    if p_value:
-        print("Terrain3DImporter: Importing files:\n\t%s\n\t%s\n\t%s" % [ height_file_name, control_file_name, color_file_name])
+	if p_value:
+		print("Terrain3DImporter: Importing files:\n\t%s\n\t%s\n\t%s" % [ height_file_name, control_file_name, color_file_name])
 
-        var imported_images: Array[Image]
-        imported_images.resize(Terrain3DRegion.TYPE_MAX)
-        var min_max := Vector2(0, 1)
-        var img: Image
-        if height_file_name:
-            img = Terrain3DUtil.load_image(height_file_name, ResourceLoader.CACHE_MODE_IGNORE, r16_range, r16_size)
-            min_max = Terrain3DUtil.get_min_max(img)
-            imported_images[Terrain3DRegion.TYPE_HEIGHT] = img
-        if control_file_name:
-            img = Terrain3DUtil.load_image(control_file_name, ResourceLoader.CACHE_MODE_IGNORE)
-            imported_images[Terrain3DRegion.TYPE_CONTROL] = img
-        if color_file_name:
-            img = Terrain3DUtil.load_image(color_file_name, ResourceLoader.CACHE_MODE_IGNORE)
-            imported_images[Terrain3DRegion.TYPE_COLOR] = img
-            if assets.get_texture_count() == 0:
-                material.show_checkered = false
-                material.show_colormap = true
-        var pos := Vector3(import_position.x, 0, import_position.y)
-        data.import_images(imported_images, pos, height_offset, import_scale)
-        print("Terrain3DImporter: Import finished")
+		var imported_images: Array[Image]
+		imported_images.resize(Terrain3DRegion.TYPE_MAX)
+		var min_max := Vector2(0, 1)
+		var img: Image
+		if height_file_name:
+			img = Terrain3DUtil.load_image(height_file_name, ResourceLoader.CACHE_MODE_IGNORE, r16_range, r16_size)
+			min_max = Terrain3DUtil.get_min_max(img)
+			imported_images[Terrain3DRegion.TYPE_HEIGHT] = img
+		if control_file_name:
+			img = Terrain3DUtil.load_image(control_file_name, ResourceLoader.CACHE_MODE_IGNORE)
+			imported_images[Terrain3DRegion.TYPE_CONTROL] = img
+		if color_file_name:
+			img = Terrain3DUtil.load_image(color_file_name, ResourceLoader.CACHE_MODE_IGNORE)
+			imported_images[Terrain3DRegion.TYPE_COLOR] = img
+			if assets.get_texture_count() == 0:
+				material.show_checkered = false
+				material.show_colormap = true
+		var pos := Vector3(import_position.x, 0, import_position.y)
+		data.import_images(imported_images, pos, height_offset, import_scale)
+		print("Terrain3DImporter: Import finished")
 
 
 func save_data(p_value: bool) -> void:
-    if destination_directory.is_empty():
-        push_error("Set destination directory first")
-        return
-    data.save_directory(destination_directory)
+	if destination_directory.is_empty():
+		push_error("Set destination directory first")
+		return
+	data.save_directory(destination_directory)
 
 
 @export_group("Export File")
@@ -96,6 +102,6 @@ enum { TYPE_HEIGHT, TYPE_CONTROL, TYPE_COLOR }
 @export var run_export: bool = false : set = start_export
 
 func start_export(p_value: bool) -> void:
-    var err: int = data.export_image(file_name_out, map_type)
-    print("Terrain3DImporter: Export error status: ", err, " ", error_string(err))
-    
+	var err: int = data.export_image(file_name_out, map_type)
+	print("Terrain3DImporter: Export error status: ", err, " ", error_string(err))
+	
