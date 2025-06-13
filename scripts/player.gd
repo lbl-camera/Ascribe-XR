e633979fe69026d5c663ff3ce5556622ea695400
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-06-13 16:13:58 -0700

    fix movement controls; rebind menu button

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends XROrigin3D
 func _on_left_hand_controller_button_pressed(name: String) -> void:
     pass
     var event = InputEventAction.new()
-    if name == 'menu_button':
+    if name == 'menu_button' or name == 'by_button':
         event.action = "ui_menu"
     event.pressed = true
     if event.action:
