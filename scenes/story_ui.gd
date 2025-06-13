81839117794b7e12077db7122a2fe52846cbcdc7
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-06-13 12:21:03 -0700

    fix story ui when specimen has no story

--- scenes/story_ui.gd
+++ scenes/story_ui.gd
@@ -7,7 +7,7 @@ var story: PackedStringArray:
         
 var page: int = 0:
     set(value):
-        if value > len(story) or value < 0:
+        if value > len(story) or value < 0 or not len(story):
             return
         
         page = value
