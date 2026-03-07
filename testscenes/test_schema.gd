ba4ef8c23194ad1220187f154a41667b886cd09f
lukiepookieee <luke.weidner4@gmail.com>
2026-03-06 16:14:01 -0800

    vertical layout

--- testscenes/test_schema.gd
+++ testscenes/test_schema.gd
@@ -7,6 +7,8 @@ extends Control
 # grid containers, VBOXContainer
 var in_range: bool = false
 @onready var container = $MarginContainer/VBoxContainer
+
+
 @export var offset: float = 50.0
 
 func setup_drop_down(enum_possibilities: Array) -> OptionButton:
@@ -16,15 +18,32 @@ func setup_drop_down(enum_possibilities: Array) -> OptionButton:
 	return drop_down
 		
 
-func set_property_types(type):
+func set_property_types(type, default):
 	# print(type)
+	
 	match type:
 		"boolean":
 			var check_box = CheckBox.new()
 			container.add_child(check_box)
+			# check_box.toggle_mode = true if default == 'true' else false
+		"number":
+			# if we're in a slider part don't bother
+			if in_range:
+				in_range = false
+			
+			
 
-func create_slider(slider_values: Array):
+func create_slider(slider_values: Array, initial_position):
+	if initial_position is String:
+		if initial_position == 'true':
+			initial_position = 1.0
+		elif initial_position == 'false':
+			initial_position = 0.0
+		else:
+			return
+	print(initial_position)
 	var slider = HSlider.new()
+	slider.ticks_on_borders = true
 	var range_container = HBoxContainer.new()
 	slider.min_value = slider_values[0]
 	var min_label = Label.new()
@@ -36,6 +55,8 @@ func create_slider(slider_values: Array):
 	container.add_child(range_container)
 	range_container.add_child(min_label)
 	range_container.add_child(max_label)
+	range_container.add_theme_constant_override("separation", 750)
+	slider.value = initial_position
 	# range_container.theme
 	
 	
@@ -44,21 +65,23 @@ func make_ui(properties: Dictionary):
 	# from here we can loop the attribute of each property, 
 	# we should get enum, type, or something to indicate range, 
 	# and then we can match off that
+	print(properties['default'])
 	var slider_values = []
 	for type in properties.keys():
 		# print(type)
 		match type:
 			"enum":
 				var drop_down_menu = setup_drop_down(properties[type])
+				drop_down_menu.selected = properties[type].find(properties['default'])
 				container.add_child(drop_down_menu)
 			"type":
-				set_property_types(properties[type])
+				set_property_types(properties[type], properties['default'])
 			"minimum":
 				slider_values.append(properties[type])
 				in_range = true
 			"maximum":
 				slider_values.append(properties[type])
-				create_slider(slider_values)
+				create_slider(slider_values, properties['default'])
 		
 				
 				
