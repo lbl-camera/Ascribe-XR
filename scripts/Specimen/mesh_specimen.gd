1718eb60ecee418c4b02453a73f1d0ccc8eddac9
lukiepookieee <luke.weidner4@gmail.com>
2026-05-27 16:51:17 -0700

    flipped normals

--- scripts/Specimen/mesh_specimen.gd
+++ scripts/Specimen/mesh_specimen.gd
@@ -42,6 +42,7 @@ func activate() -> void:
 		ui_instance.get_node("%FileDialogLayer").hide()
 		ui_instance.get_node("%SettingsLayer").show()
 		ui_instance.get_node("%MaterialMenu").show()
+		ui_instance.get_node("%FlipNormals").show()
 
 
 func _process(delta: float) -> void:
@@ -306,7 +307,37 @@ func _set_pickable(node: Node3D) -> void:
 		ui_instance.get_node("%MaterialMenu").show()
 		ui_instance.get_node("%FileDialogLayer").hide()
 		ui_instance.get_node("LoadingLayer").hide()
+		ui_instance.get_node("%FlipNormals").show()
+		var flip_node: CheckButton = ui_instance.get_node("%FlipNormals")
+		if flip_node and !flip_node.pressed.is_connected(on_flip_normals_pressed):
+			flip_node.connect("pressed", on_flip_normals_pressed)
 
+func on_flip_normals_pressed() -> void:
+	
+	if _mesh_data == null:
+		return
+
+	flip_normals = !flip_normals
+	_flip_mesh_data_normals(_mesh_data)
+	_set_mesh_from_data(_mesh_data)
+
+	if is_multiplayer_authority():
+		_send_mesh(_mesh_data.to_dict())
+
+
+func _flip_mesh_data_normals(data: MeshData) -> void:
+	# need to do reverse triangle winding for the sake of fixing the lighting
+	print(data.normals.slice(0, 6))
+	if data.indices.size() >= 3:
+		for i in range(0, data.indices.size(), 3):
+			var tmp = data.indices[i + 1]
+			data.indices[i + 1] = data.indices[i + 2]
+			data.indices[i + 2] = tmp
+
+	# the actual inverting part
+	for i in range(data.normals.size()):
+		data.normals[i] = -data.normals[i]
+	print(data.normals.slice(0, 6))
 
 func _make_pickable(node: Node3D) -> void:
 	var collision := CollisionShape3D.new()
@@ -348,7 +379,7 @@ func _send_mesh(data: Dictionary) -> void:
 	var verts = data.get('vertices', PackedFloat32Array())
 	var indices: PackedInt32Array = data.get('indices', PackedInt32Array())
 	var normals = data.get('normals', PackedFloat32Array())
-
+	# print(normals)
 	# Ensure flat arrays for chunking
 	if typeof(verts) == TYPE_PACKED_VECTOR3_ARRAY:
 		verts = MeshUtils.flatten_vector3(verts)
